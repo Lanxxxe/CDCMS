d81e68bed23943d64e3a9ef84c5e6fdcd6c599cc
Lanxxxe <Lanxxxe>
2025-03-04 23:51:39 +0800

    create: UI on guardian

--- guardian/urls.py
+++ guardian/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from guardian.views import *
 
 urlpatterns = [
-    path('', dashboard, name='guardian_dashboard'),
-    path('announcement', announcement, name='guardian_announcement'),
+    path('', announcement, name='guardian_announcement'),
+    path('attendance', attendance, name='guardian_attendance'),
+    path('grades', grades, name='guardian_grades'),
+    path('requirements', requirements, name='guardian_requirements'),
+    path('teachers_profile', teachers_profile, name='guardian_teachers_profile'),
+    path('ai_recommendation', ai_recommendation, name='guardian_ai_recommendation'),
 ]
--- guardian/views.py
+++ guardian/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
-def dashboard(request):
-    return render(request, 'guardian_dashboard.html')
 
 def announcement(request):
     return render(request, 'guardian/announcement.html')
+
+def attendance(request):
+    return render(request, 'guardian/attendance.html')
+
+def grades(request):
+    return render(request, 'guardian/grades.html')
+
+def requirements(request):
+    return render(request, 'guardian/requirements.html')
+
+def teachers_profile(request):
+    return render(request, 'guardian/teachers_profile.html')
+
+def ai_recommendation(request):
+    return render(request, 'guardian/ai_recommendation.html')
